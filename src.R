2200d561c4da737c303d8cc0df77fd77da61f317
Kira Coder Gylling <kira.gylling@gmail.com>
2021-05-10 12:19:21 +0200

    Update src.R
    Weighted logit routines updated with mustart to make the optimization more robust.

--- src.R
+++ src.R
@@ -13,7 +13,8 @@ get_stat<-function(data,covar,mediator,exposure,interactionterm,outcome,weight,M
   
   if(M==1){
   
-    outm.fit<-glm(as.formula(form_M), family=binomial(link=logit), data = data, weights=eval(parse(text=weight)))
+    outm.fit<-glm(as.formula(form_M), family=binomial(link="logit"), data = data, weights=eval(parse(text=weight)),
+                 mustart=rep(0.5, nrow(data))
     ssm<-NULL  
   }
   
@@ -31,11 +32,12 @@ get_stat<-function(data,covar,mediator,exposure,interactionterm,outcome,weight,M
   #browser()
   form_Y <- paste( outcome, "~", exposure, "+", mediator, "+", interactionterm, "+", paste(covar, collapse = " + "))
   if(O==1){
-    outy.fit<-glm(as.formula(form_Y), family=binomial(link=logit), data = data, weights=eval(parse(text=weight)))
+    outy.fit<-glm(as.formula(form_Y), family=binomial(link="logit"), data = data, weights=eval(parse(text=weight)),
+                 mustart=rep(0.5, nrow(data))
   
   }
   if(O==0){
-    outy.fit<-glm(as.formula(form_Y), family=gaussian(link=identity), data = data, weights=eval(parse(text=weight)))
+    outy.fit<-lm(as.formula(form_Y), data = data, weights=eval(parse(text=weight)))
     
   }
   
